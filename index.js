f7553564d325cd05a46bec5d54f606f6783aad88
Davy Engone <davy@hackages.io>
2021-08-23 09:56:49 +0200

    refactored the add todo feature

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+let addBtn;
+let todoInput;
+let todoList;
+
 window.addEventListener("DOMContentLoaded", () => {
   // 1 - Get a reference to elements by Ids
-  const addBtn = document.getElementById("addBtn");
+  addBtn = document.getElementById("addBtn");
+  todoInput = document.getElementById("todoInput");
+  todoList = document.getElementById("todos");
+
   const deleteBtn = document.getElementById("deleteBtn");
   const doneBtn = document.getElementById("doneBtn");
-  const todoInput = document.getElementById("todoInput");
-  const todoList = document.getElementById("todos");
 
   // 2 - Register events handlers
   addBtn.addEventListener("click", addTodo);
@@ -14,23 +19,33 @@ const todos = [];
 
 function addTodo() {
   // 1 - Get the value of the input
-  const todo = document.getElementById("todoInput").value;
+  const todo = todoInput.value;
   // 2 - Add It to the list of todos
   todos.push(todo);
-
   // 3 - Display in the visual list
-  const todoList = document.getElementById("todos");
+  displayTodo(todo);
+  todoInput.value = "";
+}
+
+function displayTodo(todoDescription) {
+  // Add li element
   const li = document.createElement("li");
-  li.innerHTML = todo;
-  const doneBtn = document.createElement("button");
-  doneBtn.textContent = "Done";
-  doneBtn.addEventListener("click", doneTodo);
-  li.append(doneBtn);
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
-  deleteButton.addEventListener("click", deleteTodo);
+  li.innerHTML = todoDescription;
+  // add Done button
+  const doneButton = createButton("Done", doneTodo);
+  li.append(doneButton);
+
+  // add Delete button
+  const deleteButton = createButton("Delete", deleteTodo);
   li.append(deleteButton);
   todoList.append(li);
+}
+
+function createButton(text, handler) {
+  const btn = document.createElement("button");
+  btn.textContent = text;
+  btn.addEventListener("click", handler);
+  return btn;
 }
 
 function doneTodo() {}
