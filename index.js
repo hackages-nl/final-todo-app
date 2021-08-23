a849d6ae9e374d615250027e72db0d808095381d
Davy Engone <davy@hackages.io>
2021-08-23 13:21:46 +0200

    refactor the app

--- index.js
+++ index.js
@@ -2,56 +2,15 @@ let addBtn;
 let todoInput;
 let todoList;
 let doneBtn;
+let doneTodoList;
 
 window.addEventListener("DOMContentLoaded", () => {
   // 1 - Get a reference to elements by Ids
   addBtn = document.getElementById("addBtn");
   todoInput = document.getElementById("todoInput");
   todoList = document.getElementById("todos");
-  doneBtn = document.getElementById("doneBtn");
-
-  const deleteBtn = document.getElementById("deleteBtn");
+  doneTodoList = document.getElementById("doneTodos");
 
   // 2 - Register events handlers
   addBtn.addEventListener("click", addTodo);
-  doneBtn.addEventListener("click", doneTodo);
 });
-
-const todos = [];
-
-function addTodo() {
-  // 1 - Get the value of the input
-  const todo = todoInput.value;
-  // 2 - Add It to the list of todos
-  todos.push(todo);
-  // 3 - Display in the visual list
-  displayTodo(todo);
-  todoInput.value = "";
-}
-
-function displayTodo(todoDescription) {
-  // Add li element
-  const li = document.createElement("li");
-  li.innerHTML = todoDescription;
-  // add Done button
-  const doneButton = createButton("Done", doneTodo.bind(null, li));
-  li.append(doneButton);
-
-  // add Delete button
-  const deleteButton = createButton("Delete", deleteTodo);
-  li.append(deleteButton);
-  todoList.append(li);
-}
-
-function createButton(text, handler) {
-  const btn = document.createElement("button");
-  btn.textContent = text;
-  btn.addEventListener("click", handler);
-  return btn;
-}
-
-function doneTodo(el) {
-  el.classList.add("done");
-}
-
-function deleteTodo() {}
